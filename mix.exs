c6a94afd9c68c31f6527fc34fc49a08d7e99f6b8
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2026-05-13 18:07:15 +0330

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule GuardedStruct.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.0-beta.1"
   @source_url "https://github.com/mishka-group/guarded_struct"
 
   def project do
